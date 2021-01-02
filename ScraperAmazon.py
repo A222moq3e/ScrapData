57ce0a3c27b21aebfc92dadf627c050c4facc95b
A222moq3e <62870638+A222moq3e@users.noreply.github.com>
2021-01-02 18:59:56 +0300

    Add files via upload

--- ScraperAmazon.py
+++ ScraperAmazon.py
@@ -4,6 +4,7 @@ import time
 from bs4 import BeautifulSoup
 import json
 import csv
+# options default value
 didchange = "n"
 optionChose = "0"
 pageRange = 500
@@ -11,12 +12,57 @@ minPrice = 0
 maxPrice = 999999999
 starzItem = "anything"
 
+# the creat class items
+
+
+class Items:
+    def __init__(self):
+        self.type = "item"
+
+
+class Methods:
+    def __init__(self):
+        self.type = "types"
+        self.pageRange = 500
+        self.minPrice = 0
+        self.maxPrice = 99 ** 5
+        self.starzItem = "anything"
+
+    def method1_PrintMethonds(self):
+        print("1: values of options")
+        print("2: change range pages for search")
+        print("2: change max and minimum price for items")
+
+    def method2_PrintNowValues(self):
+        #  print("options".center(50, "-"))
+        # range page
+        print("range of pages: ", str(self.pageRange))
+        # default price, min - max
+        print("minimum price "+str(self.minPrice))
+        print("maximum price "+str(self.maxPrice))
+        # satrz
+        print("satrz " + self.starzItem)
+        # print("hi")
+
+    def method3_NewRangePage(self, newRange):
+        self.pageRange = newRange
+        print("The new range is : ", self.pageRange)
+
+    def method4_NewMaxAndMinimum(self, newMax, newMin):
+        self.maxPrice = newMax
+        self.minPrice = newMin
+        print("the new max price is: ", self.maxPrice)
+        print("the new min price is: ", self.minPrice)
+
+
+MyMethods = Methods()
+
 
 # input search name
 print("what you whant to search: ")
 searchInput = input()
 filename = 'amazon-'+searchInput
-filecsv = open(filename+'.csv', 'w', encoding='utf8')
+
 # Set the URL you want to webscrape from
 url = 'https://www.amazon.sa/s?k='+searchInput + \
     '&i=toys&rh=n%3A12463618031&dc&crid=2J8D038E4V5IH&qid=1608914391&rnid=17120842031&sprefix=gund%2Caps%2C236&ref=sr_pg_3&page='
@@ -36,46 +82,41 @@ while didchange == "y" or didchange == "Y":
     print(optionChose)
     print(optionChose == "1")
     if optionChose == "0":  # print all options
-
-        print("1: values of options")
-        print("2: change range pages for search")
-        print("2: change max and minimum price for items")
+        MyMethods.method1_PrintMethonds()
     elif optionChose == "1":
-        print("options".center(50, "-"))
-        # range page
-        print("range of pages: "+str(pageRange))
-        # default price, min - max
-        print("minimum price "+str(minPrice))
-        print("maximum price "+str(maxPrice))
-
-        # satrz
-        print("satrz " + starzItem)
+        MyMethods.method2_PrintNowValues()
 
     elif optionChose == "2":
-        print("the new range for pages to search")
+        print("the old range page: ", MyMethods.pageRange)
+        print("the new range for pages to search: ")
         pageRange = int(input())
+        MyMethods.method3_NewRangePage(pageRange)
+
     elif optionChose == "3":
         print("the new maximum and minimum price")
         print("max parice")
         maxPrice = int(input())
         print("minimum parice")
         minPrice = int(input())
+        MyMethods.method4_NewMaxAndMinimum(maxPrice, minPrice)
+
+    elif optionChose == "99" or optionChose == "exit":
+        print("did you finish[y/n]? ")
+        didchange = input()
 
     else:
-        print("you chose wrong number chose \"0\" to see all methonds")
-    print("did you whant to continue change[y/n]")
-    didchange = input()
+        print("you chose wrong number chose \"0\" to see all methonds or 99 for exit")
 
-for page in range(pageRange):
+
+for page in range(MyMethods.pageRange):
     print('---', page, '---')
     r = requests.get(url + str(page))
     print(url + str(page))
     soup = BeautifulSoup(r.content, "html.parser")
     ancher = soup.find_all('div', {
                            'class': 'sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col sg-col-4-of-20'})
-    writer = csv.DictWriter(filecsv, fieldnames=csv_columns)
+
     i = 0
-    writer.writeheader()
 
     for pt in ancher:
 
@@ -85,18 +126,32 @@ for page in range(pageRange):
         img = pt.find('img', {'class': 's-image'})
         print('name => ', name.text)
         # print('price => ', itemPrice.text)
-        # print('img => ', img.get('src'))
 
         if img and itemPrice:
-            writer.writerow({'name': name.text.replace('                    ', '').strip(
-                '\r\n'), 'price': itemPrice.text, 'img': img.get('src')})
-            data['name'] = name.text.replace(
-                '                    ', '').strip('\r\n')
-            data['price'] = itemPrice.text
-            data['img'] = img.get('src')
-            json_data = json.dumps(data, ensure_ascii=False)
-            file.write(json_data)
-            file.write(",\n")
+            ip = itemPrice.text.split('.')
+            ip_2 = ip[0].split(',')
+            # print("range index for ip2 => ", len(ip_2))
+            if len(ip_2) > 1:
+                ip_2_sum = ip_2[0] + ip_2[1]
+            else:
+                ip_2_sum = ip[0]
+
+            # print(ip[0])
+            # print("ip_2_sum = ", ip_2_sum)
+            # print('img => ', img.get('src'))
+            if int(ip_2_sum) > MyMethods.minPrice and int(ip_2_sum) < MyMethods.maxPrice:
+
+                data['name'] = name.text.replace(
+                    '                    ', '').strip('\r\n')
+                data['price'] = itemPrice.text
+                # print('price == ', data['price'])
+                data['img'] = img.get('src')
+                json_data = json.dumps(data, ensure_ascii=False)
+                file.write(json_data)
+                file.write(",\n")
 file.write("\n]")
-filecsv.close()
+
 file.close()
+
+
+# قمت بحذف filecsv
